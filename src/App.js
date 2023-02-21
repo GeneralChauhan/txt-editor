895305a560c64e0d8a081d272941bacc03143f95
God <abhinabpratap1311@gmail.com>
2023-02-21 06:16:59 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import TextEditor from './TextEditor';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <TextEditor/>
     </div>
   );
 }
